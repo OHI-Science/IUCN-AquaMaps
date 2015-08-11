88577473e13e8f2f15e446744a8da338b212120d
jafflerbach <afflerbach@nceas.ucsb.edu>
2015-08-11 15:11:59 -0700

    added slider input and species select

--- shiny/ui.R
+++ shiny/ui.R
@@ -2,10 +2,21 @@
 
 
 shinyUI(fluidPage(
+  
+  #Application Title
   titlePanel("Comparing IUCN and AquaMaps species ranges"),
   
+  #Sidebar with controls to select the species and the aquamaps probability
+  # of occurrence cutoff point
   sidebarLayout(
-    sidebarPanel( "sidebar panel"),
+    sidebarPanel(
+      selectInput("species","Choose a species:",
+                  choices=c('A','B','C')),
+      
+      sliderInput('cutoff','Probability of Occurrence cutoff:',
+                  min=0,max=1,value=0.4,step=0.1)
+      
+      ),
     mainPanel("main panel")
   )
 ))
