e04355c03a2aa20853c3278774c258fd68b4507e
jafflerbach <afflerbach@nceas.ucsb.edu>
2015-08-11 15:03:30 -0700

    init commit

---
+++ shiny/ui.R
@@ -0,0 +1,11 @@
+#ui.r
+
+
+shinyUI(fluidPage(
+  titlePanel("Comparing IUCN and AquaMaps species ranges"),
+  
+  sidebarLayout(
+    sidebarPanel( "sidebar panel"),
+    mainPanel("main panel")
+  )
+))
