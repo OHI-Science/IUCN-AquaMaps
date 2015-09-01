fa310cfa951d9340831c7561381aea329dbf3ffb
Jamie Afflerbach <afflerbach@neptune.nceas.ucsb.edu>
2015-09-01 11:28:54 -0700

    deleted unused commented functions and added in percent overlap calculation

--- shiny/server.R
+++ shiny/server.R
@@ -14,6 +14,7 @@ iucn_cols <- colorRampPalette(brewer.pal(9, 'Purples'))(255) # purple color sche
 shinyServer(function(input,output){
   
   ### Species Map Function ###
+  
   # This function takes a single species scientific name as input, then grabs all occurrence cells and associated probability per cell
   spp_map <- reactive({
     spp <-input$species
@@ -21,41 +22,6 @@ shinyServer(function(input,output){
     spp_map <- get_spp_map(spp)
   })
   
-#   am_map_prob <- reactive({
-#     am_map <- spp_map() %>%
-#       mutate(am_pres = ifelse(am_prob >= input$am_cutoff, am_prob, NA))
-#     r_am_spp <- subs(loiczid_raster, 
-#                      am_map[ , c('loiczid', 'am_pres')], 
-#                      by = 'loiczid', 
-#                      which = 'am_pres', 
-#                      subsWithNA = TRUE)
-#     plot(r_am_spp, col = am_prob_cols, main = input$species, useRaster = FALSE)
-#     map('world', col = 'gray95', fill = T, border = 'gray80', add = TRUE)
-#   })
-  
-#   am_map_pres <- reactive({
-#     am_map <- spp_map() %>%
-#       mutate(am_pres = ifelse(am_prob >= input$am_cutoff, 1, NA))
-#     r_am_spp <- subs(loiczid_raster, 
-#                      am_map[ , c('loiczid', 'am_pres')], 
-#                      by = 'loiczid', 
-#                      which = 'am_pres', 
-#                      subsWithNA = TRUE)
-#     plot(r_am_spp, col = am_pres_cols, main = input$species, useRaster = FALSE)
-#     map('world', col = 'gray95', fill = T, border = 'gray80', add = TRUE)
-#   })
-  
-#   iucn_map <- reactive({
-#     iucn_map <- spp_map() %>%
-#       mutate(iucn_pres = ifelse(iucn_area > 0, 1, NA))
-#     r_iucn_spp <- subs(loiczid_raster, 
-#                        iucn_map[ , c('loiczid', 'iucn_pres')], 
-#                        by = 'loiczid', 
-#                        which = 'iucn_pres', 
-#                        subsWithNA = TRUE)
-#     plot(r_iucn_spp, col = iucn_cols, main = input$species, useRaster = FALSE)
-#     map('world', col = 'gray95', fill = T, border = 'gray80', add = TRUE)
-#   })
   
   map_compare <- reactive({
     
@@ -98,34 +64,41 @@ shinyServer(function(input,output){
       legend("topright",legend=c("AquaMaps","IUCN"),fill=c(am_pres_cols[128],iucn_cols[128]))
       map('world', col = 'gray95', fill = T, border = 'gray80', add = TRUE)
     }
-
-#   if(input$top == 'iucn') {
-#       plot(r_am_spp, col = am_pres_cols, main = input$species, useRaster = FALSE, alpha = input$am_trans/100)
-#       plot(r_iucn_spp, col = iucn_cols, useRaster = FALSE, alpha = input$iucn_trans/100, add = TRUE)   
-#     } else {
-#       plot(r_iucn_spp, col = iucn_cols,  main = input$species, useRaster = FALSE, alpha = input$iucn_trans/100)         
-#       plot(r_am_spp, col = am_pres_cols, useRaster = FALSE, alpha = input$am_trans/100, add = TRUE)
-#     }
-#    map('world', col = 'gray95', fill = T, border = 'gray80', add = TRUE)
-  })
-  
-  
-#   output$aquamap_prob <- renderPlot({
-#     am_map_prob()
-#   })
-#   
-#   output$aquamap_pres <- renderPlot({
-#     am_map_pres()
-#   })
-#   
-#   output$iucn <- renderPlot({
-#     iucn_map()
-#   })
   
   output$comparePlot <- renderPlot({
     map_compare()
   }, width = 1000, height = 600)
+  
+  
+  # get percent overlap of map
+  
+  #iucn map cells
+  cells_iucn<-spp_map()%>%
+    filter(iucn_pres==TRUE)%>%
+    .$loiczid
+  cells_am <- spp_map()%>%
+    filter(!is.na(am_prob))%>%
+    .$loiczid
+  
+  cells_total = unique(spp_map()$loiczid)
+  cells_overlap = intersect(cells_iucn,cells_am)
+  
+  #percent overlap
+  perc = (length(cells_overlap)/length(cells_total))*100
+  
+  am_only = setdiff(cells_am,cells_iucn)
+  iucn_only = setdiff(cells_iucn,cells_am)
+  
+  perc_am = (length(am_only)/length(cells_total))*100
+  perc_iucn = (length(iucn_only)/length(cells_total))*100
 
-
+  output$summary <- renderText({
+    
+    paste("Percent overlap is", perc,"%")
+    paste(perc_am,"% of the total area is just AquaMaps cells")
+    paste(perc_iucn,"% of the total area is just IUCN")
+    
+    
+  })
   
 })
