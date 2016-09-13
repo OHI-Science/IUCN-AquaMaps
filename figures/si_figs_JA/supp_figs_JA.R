b8cb30b4b99fab4b19846ab2fe0c09765e7b64b6
Jamie <afflerbach@nceas.ucsb.edu>
2016-09-13 14:22:52 -0700

    added new figs to show pres/absence and prob of occurrence for aquamaps data

--- figures/si_figs_JA/supp_figs_JA.R
+++ figures/si_figs_JA/supp_figs_JA.R
@@ -87,7 +87,7 @@ tuna_am_raster <- tm_shape(r_am) +
 save_tmap(tuna_am_raster, filename = 'figures/si_figs_JA/tuna_am_world.png',
           width = 19, units = "cm", dpi = 600)
 
-#get shapefile and crop to same extent
+#get tuna shapefile and crop to same extent
 
 tuna <- readOGR(dsn = file.path(dir_M,'git-annex/globalprep/_raw_data/iucn_spp/d2015/iucn_shp'),layer = 'TUNAS_BILLFISHES')
 
@@ -98,41 +98,50 @@ dir_M             <- c('Windows' = '//mazu.nceas.ucsb.edu/ohi',
 
 ta <- tuna%>%subset(binomial == 'Thunnus alalunga')
 
-tuna_wrld <- tm_shape(ta)+
-  tm_fill(col='aquamarine3', alpha = 0.8)+
-  tm_shape(wrld_simpl)+
-  tm_fill(col = 'gray')
+#albacore tuna IUCN global range as polygon
 
-save_tmap(tuna_wrld, filename ='figures/si_figs_JA/tuna_world.png',
+tuna_wrld <- tm_shape(wrld_simpl)+
+              tm_fill(border.col = 'darkgray', col = 'lightgray')+
+              tm_shape(ta)+
+              tm_fill(col='aquamarine3', alpha = 0.8)
+  
+
+save_tmap(tuna_wrld, filename ='figures/si_figs_JA/tuna_iucn_world.png',
           width = 19, units = "cm", dpi = 600)
 
+
+# Get the shapefile and crop to the northeast region
 
 ta_crop <- crop(ta,crop_ext)
 extent(ta_crop)<-crop_ext
 
 tuna_shp_crop <- tm_shape(land_crop)+
-  tm_polygons() +
-  tm_fill(col = 'gray')+
-  tm_shape(ta_crop)+
-  tm_fill(col='aquamarine3', alpha = 0.8)
+                  tm_polygons(border.col = 'darkgray', col = 'lightgray') +
+                  tm_shape(ta_crop)+
+                  tm_fill(col='aquamarine3', alpha = 0.8)
 
+#save using png since save_tmap won't save both polygons....
+png(filename = 'figures/si_figs_JA/tuna_iucn_crop.png', width = 9.5, height = 9.5, units = "cm", res = 600)
+tuna_shp_crop
+dev.off()
 
-
-save_tmap(tuna_shp_crop, filename ='figures/si_figs_JA/tuna_crop.png',
-          width = 9.5, units = "cm", dpi = 600)
+## Add grid on top of shapefile
 
 tuna_crop_grid <-tm_shape(land_crop)+
   tm_fill(col = 'gray')+
   tm_shape(ta_crop)+
   tm_fill(col='aquamarine3', alpha = 0.8)+
-  tm_grid(n.x = 60, n.y = 80, projection = 'longlat', col = 'lightgray', 
+  tm_grid(n.x = 60, n.y = 40, projection = 'longlat', col = 'lightgray', 
           labels.inside.frame = FALSE,labels.size=0)+
   tm_shape(land_crop)+
-  tm_polygons()+
-  tm_fill(col='gray')
+  tm_polygons(border.col = 'darkgray', col = 'lightgray')
 
+#save using png since save_tmap won't save both polygons....
+png(filename = 'figures/si_figs_JA/tuna_iucn_crop_grid.png', width = 9.5, height = 9.5, units = "cm", res = 600)
+tuna_crop_grid
+dev.off()
 
-save_tmap(tuna_crop_grid,filename = 'figures/si_figs_JA/tuna_crop_grid.png',
+save_tmap(tuna_crop_grid,filename = 'figures/si_figs_JA/tuna_iucn_crop_grid.png',
           width = 9.5, units = "cm", dpi = 600)
 
 
@@ -142,9 +151,6 @@ ta_crop <- crop(ta,extent(0,180,-90,90))
 
 r_na <- raster(vals=NA,ext=extent(0,180,-90,90), res = 0.5)
 r_iucn_half <- r_iucn%>%crop(extent(-180,0,-90,90))%>%merge(r_na)
-plot(r_iucn_half, box = F, axes = F, legend = F)
-plot(ta_crop, add = T, col = 'darkgreen')
-
 
 tuna_iucn_world <- tm_shape(r_iucn_half)+
                     tm_raster(palette = 'indianred3',
@@ -156,5 +162,67 @@ tuna_iucn_world <- tm_shape(r_iucn_half)+
                     tm_shape(wrld_simpl) +
                     tm_fill(col = 'gray')
 
+
 save_tmap(tuna_iucn_world, filename = 'figures/si_figs_JA/tuna_world_map_IUCN_raster_poly.png',
+          width = 19, units = "cm", dpi = 600)
+
+#create world map from aquamaps data. Half the world showing probability of occurrence, other half just presence/absence
+
+
+source('data_explore/mapping aquamaps.R')
+
+#pres/abs
+r_pres <- r_am%>%crop(extent(c(0,180,-90,90)))
+
+#prob occurrence
+
+# get spp_cells raw data for aquamaps
+dir_aquamaps <- file.path(dir_M, 'git-annex/globalprep/_raw_data/aquamaps/d2015')
+
+spp_cells <- read_csv(file.path(dir_aquamaps, 'csv/hcaf_sp_native_trunc.csv'), 
+                      col_types = 'ccd', progress = TRUE)
+
+spp <- fread(input = file.path(dir_aquamaps, 'csv/speciesoccursum.csv'), 
+             verbose = FALSE, header = TRUE) %>%
+  mutate(sciname = paste(genus, species, sep = ' '),
+         iucn_code = str_replace(toupper(iucn_code), 'LR/', ''),
+         iucn_code = str_replace(iucn_code, 'N.E.', 'NE'))
+
+spp <- as.data.frame(spp)
+
+#get species id
+sp_id  = filter(spp,sciname=='Thunnus alalunga')$speciesid
+
+#get prob of occurrence cells
+sp_map = filter(spp_cells,speciesid==sp_id)
+
+#turn dataframe into raster
+
+r_sp = subs(loiczid_raster, sp_map[,c('loiczid','probability')], by='loiczid', which='probability', subsWithNA=T)
+
+#combine with pres/absence raster
+
+
+tuna_am_combine <- tm_shape(r_sp)+
+  tm_raster(palette = 'YlOrRd',
+            colorNA = NULL,
+            alpha = 1,
+            legend.show=TRUE,
+            title = "Probability of Occurrence") +
+  tm_layout(legend.text.size = .8, #Casey's default from coral maps was .5 - pretty small for this map
+            legend.title.size =.9, #default from coral maps was .6
+            # title.position = 'TOP', 
+            legend.outside = FALSE, 
+            legend.position = c('left', 'top'),
+            legend.bg.color = 'white',
+            legend.bg.alpha = .5)+
+  tm_shape(r_pres)+
+  tm_raster(palette = "darkcyan",
+            colorNA = NULL,
+            alpha = 1,
+            legend.show = FALSE) +
+  tm_shape(wrld_simpl) +
+  tm_fill(col = 'gray')
+
+save_tmap(tuna_am_combine, filename = 'figures/si_figs_JA/tuna_world_map_AM_combine.png',
           width = 19, units = "cm", dpi = 600)
